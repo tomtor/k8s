865d0389fde07d4465fa1a3322ae04a7c85847a3
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-02-13 12:16:43 +0000

    Start with python3 web app

---
+++ docker-python/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+redis_host = os.getenv("REDIS", "redis-master")
+my_port = int(os.getenv("PORT", "80"))
+
+# Connect to Redis
+redis = Redis(host=redis_host, db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=my_port)
