1cbc1e69d705774ab8911923bdbdffefcbb6db9c
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-03-20 16:31:08 +0000

    Update renew script

---
+++ renew-cert.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+microk8s.disable ingress
+
+sudo certbot renew
+
+sudo cp /etc/letsencrypt/live/tom-kad-ssh.westeurope.cloudapp.azure.com/cert.pem /tmp
+sudo cp /etc/letsencrypt/live/tom-kad-ssh.westeurope.cloudapp.azure.com/privkey.pem /tmp
+sudo chmod 644 /tmp/*.pem
+
+kubectl delete secrets testsecret-tls
+kubectl create secret tls testsecret-tls --key /tmp/privkey.pem --cert /tmp/cert.pem
+
+sudo rm /tmp/*.pem
+
+microk8s.enable ingress
