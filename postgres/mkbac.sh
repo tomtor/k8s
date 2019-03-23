3a6c5b525c916109076d788fc698527c9e7dd230
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-03-23 11:31:16 +0000

    Read postgres port in backup script

--- postgres/mkbac.sh
+++ postgres/mkbac.sh
@@ -2,11 +2,15 @@ DATE=$(date  --rfc-3339=date)
 
 echo $DATE
 
+PORT=$(kubectl get svc postgres -o=jsonpath='{.spec.ports[?(@.port==5432)].nodePort}')
+
+echo PORT $PORT
+
 # pg_dump -Fc -h localhost -U tom -p 31082 tom | pv | az storage blob upload  --container-name public --account-name tomvcool --file /dev/stdin --name "tom.$DATE.backup"
 
-pg_dump -Fc -Z 1 -h localhost -U postgresadmin -p 31082 tom | pv | azbak --no-suffix - "/public/tom.$DATE.backup"
+#pg_dump -Fc -Z 1 -h localhost -U postgresadmin -p $PORT tom | pv | azbak --no-suffix - "/public/tom.$DATE.backup"
 
-#pg_dump -Fc -Z 1 -h localhost -U postgresadmin -p 31082 nlextract | pv | azbak --no-suffix - "/public/nlextract.$DATE.backup"
+pg_dump -Fc -Z 1 -h localhost -U postgresadmin -p $PORT nlextract | pv | azbak --no-suffix - "/public/nlextract.$DATE.backup"
 
 exit 0
 
