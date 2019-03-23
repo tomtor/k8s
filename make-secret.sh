3150fe5f784aa16d6c206a5e7bd9264e3d2b38b7
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-03-23 13:30:20 +0000

    Use fullchain for certificate

--- make-secret.sh
+++ make-secret.sh
@@ -1,11 +1,11 @@
 HOSTNAME=tom-kad-k8s.westeurope.cloudapp.azure.com
 
-sudo certbot certonly --standalone -d $HOSTNAME
+sudo certbot certonly --standalone -d $HOSTNAME -d k8s.v7f.eu
 
-sudo cp /etc/letsencrypt/live/$HOSTNAME/cert.pem /tmp
+sudo cp /etc/letsencrypt/live/$HOSTNAME/fullchain.pem /tmp
 sudo cp /etc/letsencrypt/live/$HOSTNAME/privkey.pem /tmp
 sudo chown $USER /tmp/*.pem
 
-kubectl create secret tls testsecret-tls --key /tmp/privkey.pem --cert /tmp/cert.pem
+kubectl create secret tls testsecret-tls --key /tmp/privkey.pem --cert /tmp/fullchain.pem
 
 sudo rm /tmp/*.pem
--- renew-cert.sh
+++ renew-cert.sh
@@ -8,12 +8,12 @@ microk8s.disable ingress
 
 sudo certbot renew
 
-sudo cp /etc/letsencrypt/live/$HOSTNAME/cert.pem /tmp
+sudo cp /etc/letsencrypt/live/$HOSTNAME/fullchain.pem /tmp
 sudo cp /etc/letsencrypt/live/$HOSTNAME/privkey.pem /tmp
 sudo chown $USER /tmp/*.pem
 
 kubectl delete secrets testsecret-tls
-kubectl create secret tls testsecret-tls --key /tmp/privkey.pem --cert /tmp/cert.pem
+kubectl create secret tls testsecret-tls --key /tmp/privkey.pem --cert /tmp/fullchain.pem
 
 sudo rm /tmp/*.pem
 
