a4454af382fa1ee8e5338ca26cf1a973f57770f1
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-03-24 16:29:26 +0000

    fine tune script

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+HOSTNAME=tom-kad-k8s.westeurope.cloudapp.azure.com
+
 PATH=/snap/bin:$PATH
 
-HOSTNAME=tom-kad-k8s.westeurope.cloudapp.azure.com
+CRDIR=/tmp/cert_renew_$$
+
+mkdir $CRDIR
 
 sudo certbot renew --pre-hook "microk8s.disable ingress" --post-hook "microk8s.enable ingress"
 
-sudo cp /etc/letsencrypt/live/$HOSTNAME/fullchain.pem /tmp
-sudo sh -c "umask 377; cp /etc/letsencrypt/live/$HOSTNAME/privkey.pem /tmp"
-sudo chown $USER /tmp/*.pem
+sudo cp /etc/letsencrypt/live/$HOSTNAME/fullchain.pem $CRDIR
+sudo sh -c "umask 077; cp /etc/letsencrypt/live/$HOSTNAME/privkey.pem $CRDIR"
+sudo chown $USER $CRDIR/*.pem
 
-kubectl delete secrets testsecret-tls
-kubectl create secret tls testsecret-tls --key /tmp/privkey.pem --cert /tmp/fullchain.pem
+kubectl create secret tls testsecret-tls --key $CRDIR/privkey.pem --cert $CRDIR/fullchain.pem --dry-run -o yaml | kubectl apply -f -
 
-sudo rm /tmp/*.pem
+rm -rf $CRDIR
