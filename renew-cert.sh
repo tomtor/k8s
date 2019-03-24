a90ef93c68664bc32f01327f2e76b60c632e9e81
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-03-24 16:16:24 +0000

    fix security for renew certificate script
    and minimize out of service window when renewing

--- renew-cert.sh
+++ renew-cert.sh
@@ -4,17 +4,13 @@ PATH=/snap/bin:$PATH
 
 HOSTNAME=tom-kad-k8s.westeurope.cloudapp.azure.com
 
-microk8s.disable ingress
-
-sudo certbot renew
+sudo certbot renew --pre-hook "microk8s.disable ingress" --post-hook "microk8s.enable ingress"
 
 sudo cp /etc/letsencrypt/live/$HOSTNAME/fullchain.pem /tmp
-sudo cp /etc/letsencrypt/live/$HOSTNAME/privkey.pem /tmp
+sudo sh -c "umask 377; cp /etc/letsencrypt/live/$HOSTNAME/privkey.pem /tmp"
 sudo chown $USER /tmp/*.pem
 
 kubectl delete secrets testsecret-tls
 kubectl create secret tls testsecret-tls --key /tmp/privkey.pem --cert /tmp/fullchain.pem
 
 sudo rm /tmp/*.pem
-
-microk8s.enable ingress
