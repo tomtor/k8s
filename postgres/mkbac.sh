1915dcae04be7d951cef1047b1472bdba9f9a4fa
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-04-07 09:37:27 +0000

    Update configs

--- postgres/mkbac.sh
+++ postgres/mkbac.sh
@@ -6,11 +6,15 @@ PORT=$(kubectl get svc postgres -o=jsonpath='{.spec.ports[?(@.port==5432)].nodeP
 
 echo PORT $PORT
 
+pg_dumpall -r -h localhost -U postgresadmin -p $PORT > roles.sql
+
 # pg_dump -Fc -h localhost -U tom -p 31082 tom | pv | az storage blob upload  --container-name public --account-name tomvcool --file /dev/stdin --name "tom.$DATE.backup"
 
 #pg_dump -Fc -Z 1 -h localhost -U postgresadmin -p $PORT tom | pv | azbak --no-suffix - "/public/tom.$DATE.backup"
 
-pg_dump -Fc -Z 1 -h localhost -U postgresadmin -p $PORT nlextract | pv | azbak --no-suffix - "/public/nlextract.$DATE.backup"
+#pg_dump -Fc -Z 1 -h localhost -U postgresadmin -p $PORT nlextract | pv | azbak --no-suffix - "/public/nlextract.$DATE.backup"
+
+#pg_dump -Fc -Z 1 -h localhost -U postgresadmin -p $PORT -n bagactueel tom | pv | azbak --no-suffix - "/public/bag.$DATE.backup"
 
 exit 0
 
