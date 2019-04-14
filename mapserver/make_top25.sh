c703039598c5a1b7482a6bfdd994249e6dadf592
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-04-14 16:07:56 +0000

    Add tiling to overview

--- mapserver/make_top25.sh
+++ mapserver/make_top25.sh
@@ -9,3 +9,6 @@ do
 done
 
 gdalwarp -srcnodata "255 255 255" -tr 32 32 -overwrite top25/*-2*tif top25-32.tif
+gdal_translate -co TILED=YES top25-32.tif tiled.tif
+mv tiled.tif top25-32.tif
+gdaladdo -r average top25-32.tif 2 4 8 16 32
