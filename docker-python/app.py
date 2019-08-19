f502f459e2e9a9eb2d67974e810eb4ccac89793a
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-08-19 11:05:36 +0000

    Add postgres query to Python app

--- docker-python/app.py
+++ docker-python/app.py
@@ -3,6 +3,31 @@ from redis import Redis, RedisError
 import os
 import socket
 
+import psycopg2
+
+def get_delays():
+  try:
+    connection = psycopg2.connect(user = "tom",
+                                  password = os.getenv("PGPASS"),
+                                  host = os.getenv("PGHOST", "localhost"),
+                                  port = os.getenv("PGPORT", "5432"), database = "tom")
+    cursor = connection.cursor()
+    cursor.execute("select t, d from (select t, t - lag(t) over() as d \
+        from hartbeat) as ss where extract(seconds from d) > 61;")
+    d = cursor.fetchall()
+    if d:
+        result = ""
+        for r in d:
+            result += (str(r[0]) + ' ' + str(r[1]) + '<br/>')
+    else:
+        result = "No delays"
+    return result
+  finally:
+    if connection:
+        cursor.close()
+        connection.close()
+
+
 redis_host = os.getenv("REDIS", "redis-master")
 redis_password = os.getenv("REDIS_PASSWORD")
 
@@ -22,7 +47,10 @@ def hello():
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
+           "<b>Visits:</b> {visits}" \
+           "<p>" \
+           + get_delays() \
+           + "</p>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
