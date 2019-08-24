573189e7699fdc4a6e8882b4c516ed9cb5072e4a
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-08-24 13:14:36 +0200

    first run

--- DT/build.py
+++ DT/build.py
@@ -1,9 +1,30 @@
 # from https://gis.stackexchange.com/questions/230994/creating-simple-citygml-3d-models-based-on-2d-shapefiles-alkis-shp2gml-using-p
 
-import shapefile
+# import shapefile
+import os
+import psycopg2
+
+import shapely
+from shapely import wkb
+
+import geos
+from shapely.geometry import LineString
+
 from lxml import etree, objectify
 
-def build_gml_main():    
+connection = psycopg2.connect(user="tom",
+                              password=os.getenv("PGPASS"),
+                              host=os.getenv("PGHOST", "localhost"),
+                              sslmode='require',
+                              port=os.getenv("PGPORT", "5432"),
+                              database="bag3d")
+
+class reg(object):
+    def __init__(self, cursor, row):
+        for (attr, val) in zip((d[0] for d in cursor.description), row) :
+            setattr(self, attr, val)
+
+def build_gml_main():
     # define Namespaces
     ns_core = "http://www.opengis.net/citygml/1.0"
     ns_bldg = "http://www.opengis.net/citygml/building/1.0"
@@ -15,7 +36,6 @@ def build_gml_main():
     ns_schemaLocation = "http://www.opengis.net/citygml/1.0 http://schemas.opengis.net/citygml/1.0/cityGMLBase.xsd http://www.opengis.net/citygml/building/1.0 http://schemas.opengis.net/citygml/building/1.0/building.xsd http://www.opengis.net/citygml/generics/1.0 http://schemas.opengis.net/citygml/generics/1.0/generics.xsd http://www.opengis.net/gml http://schemas.opengis.net/gml/3.1.1/base/gml.xsd"
 
     #ns_core, ns_bldg, ns_gen, ns_gml, ns_xAL, ns_xlink, ns_xsi
-
 
     nsmap = {
         'core': ns_core,
@@ -38,117 +58,150 @@ def build_gml_main():
     # Add branch
     bounded = etree.SubElement(cityModel, "{%s}boundedBy" % ns_gml)
     # Add branch to a branch
-    envelop = etree.SubElement(bounded, "{%s}Envelope" % ns_gml, srsName="urn:adv:crs:ETRS89_UTM32*DE_DHHN92_NH")
-    lb = etree.SubElement(envelop, "{%s}lowerCorner" % ns_gml, srsDimension="3")
+    envelop = etree.SubElement(
+        bounded, "{%s}Envelope" % ns_gml, srsName="urn:adv:crs:ETRS89_UTM32*DE_DHHN92_NH")
+    lb = etree.SubElement(envelop, "{%s}lowerCorner" %
+                          ns_gml, srsDimension="3")
     lb.text = ''
-    ub = etree.SubElement(envelop, "{%s}upperCorner" % ns_gml, srsDimension="3")
+    ub = etree.SubElement(envelop, "{%s}upperCorner" %
+                          ns_gml, srsDimension="3")
     ub.text = ''
 
     # Read Shapefile
-    shp_layer = read_shape()
+    #shp_layer = read_shape()
+    cursor = connection.cursor()
+    cursor.execute(
+        "select identificatie, geovlak, \"roof-0.50\" as roof50 \
+        from \"3dbag\".pand3d where tile_id = '07fz1' limit 10")
 
     # Add buildings
-    cityModel, point_max, point_min = iteration_buildings(cityModel, shp_layer, ns_core, ns_bldg, ns_gen, ns_gml, ns_xAL, ns_xlink, ns_xsi)
+    cityModel, point_max, point_min = iteration_buildings(
+        cityModel, cursor, ns_core, ns_bldg, ns_gen, ns_gml, ns_xAL, ns_xlink, ns_xsi)
 
-    lb.text = str(point_min[0]) + ' ' + str(point_min[1])+ ' ' + str(point_min[2])
-    ub.text = str(point_max[0]) + ' ' + str(point_max[1])+ ' ' + str(point_max[2])
+    lb.text = str(point_min[0]) + ' ' + \
+        str(point_min[1]) + ' ' + str(point_min[2])
+    ub.text = str(point_max[0]) + ' ' + \
+        str(point_max[1]) + ' ' + str(point_max[2])
 
     # pretty print
-    print etree.tostring(cityModel, pretty_print=True)
+    pretty = etree.tostring(cityModel, pretty_print=True)
+    print(pretty)
 
     # Save File
-    et = etree.ElementTree(cityModel)
-    outFile = open('/yourPath/output_b.xml', 'w')
-    et.write(outFile, xml_declaration=True, encoding='utf-8', pretty_print= True)
-    print 'done'
+    #et = etree.ElementTree(cityModel)
+    outFile = open('output_b.xml', 'w')
+    #et.write(outFile, xml_declaration=True,
+             #encoding='utf-8', pretty_print=True)
+    outFile.write(pretty.decode('utf-8'))
+    print('done')
 
 
-def iteration_buildings(cityModel, shp_layer, ns_core, ns_bldg, ns_gen, ns_gml, ns_xAL, ns_xlink, ns_xsi):
-    #lower corner
+def iteration_buildings(cityModel, cursor, ns_core, ns_bldg, ns_gen, ns_gml, ns_xAL, ns_xlink, ns_xsi):
+    # lower corner
     point_min = None
-    #upper corner
+    # upper corner
     point_max = None
 
-    building_count = len(shp_layer.shapes())
-    field_content = shp_layer.records()
+    # building_count = len(shp_layer.shapes())
+    # field_content = shp_layer.records()
 
     # iteration
-    for i_build in range(building_count):
-
-        cityObject = etree.SubElement(cityModel, "{%s}cityObjectMember" % ns_core)
+    for i_build in cursor.fetchall():  # range(building_count):
+        r = reg(cursor, i_build)
+        print(r.identificatie)
+        cityObject = etree.SubElement(
+            cityModel, "{%s}cityObjectMember" % ns_core)
 
         # building shape area
-        points_2D = shp_layer.shapes()[i_build].points
+        points_2D = wkb.loads(r.geovlak, hex=True).exterior.coords # shp_layer.shapes()[i_build].points
+        # print(points_2D[0])
 
         # contents which are given in the shapefile and needed in the citygml model
-        inits = building_inits(field_content[i_build], shp_layer)
+        #inits = building_inits(field_content[i_build], shp_layer)
 
         # for Citygml the lower and upper limit of all buildings are needed
-        point_min, point_max = find_lower_upper_corner(points_2D, inits['dachhoehe'], point_min, point_max)
+        point_min, point_max = find_lower_upper_corner(
+            points_2D, r.roof50, point_min, point_max)
 
         # calculation of the polygon by useing the points from the building shape area
-        polygon = polygon_caculation(inits, points_2D)
+        polygon = polygon_calculation(r, points_2D)
 
         # Add branch and sub-branche of a building
-        bldg = etree.SubElement(cityObject, "{%s}Building" % ns_bldg, {"{%s}id" % ns_gml: inits['gml_id']})
+        bldg = etree.SubElement(cityObject, "{%s}Building" % ns_bldg, {
+                                "{%s}id" % ns_gml: r.identificatie})
         creationDate = etree.SubElement(bldg, "{%s}creationDate" % ns_core)
         creationDate.text = '2017-02-04'
-        externalReference = etree.SubElement(bldg, "{%s}externalReference" % ns_core)
-        informationSystem = etree.SubElement(externalReference, "{%s}informationSystem" % ns_core)
+        externalReference = etree.SubElement(
+            bldg, "{%s}externalReference" % ns_core)
+        informationSystem = etree.SubElement(
+            externalReference, "{%s}informationSystem" % ns_core)
         informationSystem.text = "http://www.BananenBAUM2017.de"
-        externalObject = etree.SubElement(externalReference, "{%s}externalObject" % ns_core)
+        externalObject = etree.SubElement(
+            externalReference, "{%s}externalObject" % ns_core)
         name = etree.SubElement(externalObject, "{%s}name" % ns_core)
-        name.text = inits['gml_id']
+        name.text = r.identificatie
 
-        stringAttribute = etree.SubElement(bldg, "{%s}stringAttribute" % ns_gen, name="Gemeindeschluessel")
-        value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
-        value.text = inits['bezirk']
+        # stringAttribute = etree.SubElement(
+        #     bldg, "{%s}stringAttribute" % ns_gen, name="Gemeindeschluessel")
+        # value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
+        # value.text = inits['bezirk']
 
-        stringAttribute = etree.SubElement(bldg, "{%s}stringAttribute" % ns_gen, name="DatenquelleDachhoehe")
-        value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
-        value.text = str(1000)
+        # stringAttribute = etree.SubElement(
+        #     bldg, "{%s}stringAttribute" % ns_gen, name="DatenquelleDachhoehe")
+        # value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
+        # value.text = str(1000)
 
-        stringAttribute = etree.SubElement(bldg, "{%s}stringAttribute" % ns_gen, name="DatenquelleLage")
-        value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
-        value.text = str(1000)
+        # stringAttribute = etree.SubElement(
+        #     bldg, "{%s}stringAttribute" % ns_gen, name="DatenquelleLage")
+        # value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
+        # value.text = str(1000)
 
-        stringAttribute = etree.SubElement(bldg, "{%s}stringAttribute" % ns_gen, name="DatenquelleBodenhoehe")
-        value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
-        value.text = str(1300)
+        # stringAttribute = etree.SubElement(
+        #     bldg, "{%s}stringAttribute" % ns_gen, name="DatenquelleBodenhoehe")
+        # value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
+        # value.text = str(1300)
 
-        stringAttribute = etree.SubElement(bldg, "{%s}stringAttribute" % ns_gen, name="BezugspunktDach")
-        value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
-        value.text = str(2100)
+        # stringAttribute = etree.SubElement(
+        #     bldg, "{%s}stringAttribute" % ns_gen, name="BezugspunktDach")
+        # value = etree.SubElement(stringAttribute, "{%s}value" % ns_gen)
+        # value.text = str(2100)
 
-        function = etree.SubElement(bldg, "{%s}function" % ns_bldg)
-        function.text = str(inits['funktion'])
+        # function = etree.SubElement(bldg, "{%s}function" % ns_bldg)
+        # function.text = str(inits['funktion'])
 
-        measuredHeight = etree.SubElement(bldg, "{%s}measuredHeight" % ns_bldg, uom="urn:adv:uom:m")
-        measuredHeight.text = str(inits['dachhoehe'])
+        measuredHeight = etree.SubElement(
+            bldg, "{%s}measuredHeight" % ns_bldg, uom="urn:adv:uom:m")
+        measuredHeight.text = str(r.roof50)
 
-        storeysAboveGround = etree.SubElement(bldg, "{%s}storeysAboveGround" % ns_bldg)
-        storeysAboveGround.text = str(inits['Anz_O'])
+        # storeysAboveGround = etree.SubElement(
+        #     bldg, "{%s}storeysAboveGround" % ns_bldg)
+        # storeysAboveGround.text = str(inits['Anz_O'])
 
         # Add the 3d polygon
         lod1Solid = etree.SubElement(bldg, "{%s}lod1Solid" % ns_bldg)
         Solid = etree.SubElement(lod1Solid, "{%s}Solid" % ns_gml)
         exterior = etree.SubElement(Solid, "{%s}exterior" % ns_gml)
-        CompositeSurface = etree.SubElement(exterior, "{%s}CompositeSurface" % ns_gml)
+        CompositeSurface = etree.SubElement(
+            exterior, "{%s}CompositeSurface" % ns_gml)
         for poly in polygon:
-            surfaceMember = etree.SubElement(CompositeSurface, "{%s}surfaceMember" % ns_gml)
-            polygon = etree.SubElement(surfaceMember, "{%s}Polygon" % ns_gml, {"{%s}id" % ns_gml: inits['gml_id']})
+            surfaceMember = etree.SubElement(
+                CompositeSurface, "{%s}surfaceMember" % ns_gml)
+            polygon = etree.SubElement(surfaceMember, "{%s}Polygon" % ns_gml, {
+                                       "{%s}id" % ns_gml: r.identificatie})
             exterior = etree.SubElement(polygon, "{%s}exterior" % ns_gml)
             LinearRing = etree.SubElement(exterior, "{%s}LinearRing" % ns_gml)
 
             for point in poly:
-                pos = etree.SubElement(LinearRing, "{%s}pos" % ns_gml, srsDimension = "3")
-                pos.text = str(point[0]) + ' ' + str(point[1])+ ' ' + str(point[2])
-            #print etree.tostring(cityModel, pretty_print=True)
-        #print etree.tostring(cityModel, pretty_print=True)
+                pos = etree.SubElement(
+                    LinearRing, "{%s}pos" % ns_gml, srsDimension="3")
+                pos.text = str(point[0]) + ' ' + \
+                    str(point[1]) + ' ' + str(point[2])
+            # print etree.tostring(cityModel, pretty_print=True)
+        # print etree.tostring(cityModel, pretty_print=True)
 
-        print 'done'
+        print('done')
 
-    #print etree.tostring(cityModel, pretty_print=True)
+    # print etree.tostring(cityModel, pretty_print=True)
     return cityModel, point_max, point_min
 
 
@@ -187,22 +240,22 @@ def building_inits(content, shp_layer):
     if int(inits['Anz_O']):
         inits['dachhoehe'] = (int(inits['Anz_O']) + 1) * 2.53
     else:
-        inits['dachhoehe'] = 3.113    
+        inits['dachhoehe'] = 3.113
     return inits
 
 
-def read_shape():
-    shp_read = shapefile.Reader('/yourPath/ALKIS 2017-01/Harburg_corp')
-    return shp_read
+# def read_shape():
+#     shp_read = shapefile.Reader('/yourPath/ALKIS 2017-01/Harburg_corp')
+#     return shp_read
 
 
-def polygon_caculation(inits, points_2D):
+def polygon_calculation(inits, points_2D):
 
     anz_polygone = len(points_2D)+2
     polygon = []
     grundhoehe = 0
     # extimated roof heigth
-    dachhoehe = inits['dachhoehe'] + grundhoehe
+    dachhoehe = inits.roof50 + grundhoehe
 
     for point_A, point_B in zip(points_2D[:-1], points_2D[1:]):
         surface = []
@@ -214,7 +267,7 @@ def polygon_caculation(inits, points_2D):
 
         polygon.append(surface)
 
-        print point_A, point_B
+        print(point_A, point_B)
 
     # add roof, add ground
     roof = []
@@ -229,7 +282,7 @@ def polygon_caculation(inits, points_2D):
 
 
 def find_lower_upper_corner(points_2D, dachhoehe, point_min, point_max):
-    #compare the given points with the saved lower and upper limit
+    # compare the given points with the saved lower and upper limit
     # if lower or upper points exist, overwrite the saved ones
     points_2D_list = list(points_2D)
     if point_min is None:
