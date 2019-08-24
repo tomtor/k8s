4ae6a07f615f7c4423e705be1fc2e95c75a630e2
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-08-24 14:57:36 +0000

    Updates

--- DT/tokml.sh
+++ DT/tokml.sh
@@ -3,7 +3,9 @@ PGUSER=tom
 PGDB=bag3d
 PGPORT=30258
 
-ogr2ogr -f "KML" mykml.kml PG:"host=$PGHOST port=$PGPORT user=$PGUSER dbname=$PGDB" \
-        -s_srs EPSG:28992 -t_srs EPSG:4326 \
-	-sql "select geovlak from \"3dbag\".pand3d where tile_id = '07fz1'"
+ogr2ogr -f "GML" mykml.gml PG:"host=$PGHOST port=$PGPORT user=$PGUSER dbname=$PGDB" \
+        "3dbag.pand3d" -s_srs EPSG:28992 -t_srs EPSG:4326 \
+	-where "tile_id = '07fz1'"
+
+#	-sql "select geovlak from \"3dbag\".pand3d where tile_id = '07fz1'"
 
