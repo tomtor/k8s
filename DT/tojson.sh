f2c0e9850fe6e01fabb612de44fc67319db500ac
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-08-27 08:25:33 +0000

    Working JSON

--- DT/tojson.sh
+++ DT/tojson.sh
@@ -6,9 +6,8 @@ PGPORT=30258
 GEMEENTE="0308"
 
 ogr2ogr -f "GeoJSON" $GEMEENTE.json PG:"host=$PGHOST port=$PGPORT user=$PGUSER dbname=$PGDB" \
-        "3dbag.pand3d" -s_srs EPSG:28992 -t_srs EPSG:28992 \
-	-select identificatie,geovlak,ground-0.50,roof-0.50 \
-	-where "gemeentecode = '"$GEMEENTE"'"
+        -s_srs EPSG:28992 -t_srs EPSG:4326 \
+	-sql "select identificatie,geovlak,\"ground-0.50\" as ground50, \"roof-0.50\" as roof50 from \"3dbag\".pand3d where gemeentecode = '"$GEMEENTE"'"
 
-#	-sql "select geovlak from \"3dbag\".pand3d where tile_id = '07fz1'"
+cp $GEMEENTE.json /data/pythonapp/public/DT/data
 
