545f24245a3d57e8d842f0e95279b5c81bcd5a72
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-08-27 12:46:43 +0000

    3D updates

--- DT/tojson.sh
+++ DT/tojson.sh
@@ -3,11 +3,12 @@ PGUSER=tom
 PGDB=bag3d
 PGPORT=30258
 
-GEMEENTE="0308"
+GEMEENTE="0080" # Leeuwarden
+GEMEENTE="0308" # Baarn
 
 ogr2ogr -f "GeoJSON" $GEMEENTE.json PG:"host=$PGHOST port=$PGPORT user=$PGUSER dbname=$PGDB" \
         -s_srs EPSG:28992 -t_srs EPSG:4326 \
-	-sql "select identificatie,geovlak,\"ground-0.50\" as ground50, \"roof-0.50\" as roof50 from \"3dbag\".pand3d where gemeentecode = '"$GEMEENTE"'"
+	-sql "select identificatie,openbareruimtenaam,huisnummer,woonplaatsnaam,st_force2d(geovlak),\"ground-0.50\" as ground50, \"roof-0.50\" as roof50 from \"3dbag\".pand3d, bagactueel.pand_compleet where identificatie = pandid and gemeentecode = '"$GEMEENTE"'"
 
 cp $GEMEENTE.json /data/pythonapp/public/DT/data
 
