47f4f66ff81c9c2acb1a4e7c6f643f51999283d5
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-01-09 13:15:04 +0000

    First production version

--- docker-python/app.py
+++ docker-python/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 from redis import Redis, RedisError
 from waitress import serve
 
@@ -61,11 +62,20 @@ redis_password = os.getenv("REDIS_PASSWORD")
 
 my_port = int(os.getenv("PORT", "80"))
 
+if my_port == 8888:
+    UPLOAD_FOLDER = '/tmp'
+else:
+    UPLOAD_FOLDER = '/data/archi'
+print("Serving from:", UPLOAD_FOLDER)
+
+ALLOWED_EXTENSIONS = {'archimate' }
+
 # Connect to Redis
 redis = Redis(host=redis_host, password=redis_password, db=0,
               socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__, static_url_path="/", static_folder='/data/public')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/public/<path:path>')
 def send_js(path):
@@ -91,8 +101,86 @@ def status():
 def hello():
     return "Nothing here..."
 
+import xml.etree.cElementTree as ET
+import urllib.request
+import uuid
+
+
+def update_archi(filename):
+    response = urllib.request.urlopen(
+        'https://raw.githubusercontent.com/tomtor/archi-test/master/Kadaster-Repository.archimate')
+    new_repo = response.read()
+    new_repo = ET.fromstring(new_repo)
+
+    data = ET.parse(UPLOAD_FOLDER + "/" + filename)
+    root = data.getroot()
+    root.attrib["xmlns:archimate"] = "http://www.archimatetool.com/archimate"
+
+    old_repo = root.find(".//folder[@name='Kadaster Repository']")
+    if old_repo:
+        root.remove(old_repo)
+    else:
+        print("no old repo")
+    root.insert(0, new_repo.find(".//folder[@name='Kadaster Repository']"))
+
+    for e in root.findall(".//child[@archimateElement]"):
+        aelem = e.get("archimateElement")
+        if not root.findall(".//element[@id='" + aelem + "']"):
+            print("Removed: ", aelem)
+            old = root.find(".//folder[@name='OLD-Repo']")
+            if not old:
+                print("Create OLD-Repo folder")
+                old = ET.SubElement(root, "folder")
+                old.attrib["name"] = "OLD-Repo"
+                old.attrib["id"] = str(uuid.uuid4())
+                old.attrib["type"] = "other"
+                root.append(old)
+            aelem = old_repo.find(".//element[@id='" + aelem + "']")
+            old.append(aelem)
+
+    tree = ET.ElementTree(root)
+
+    tree.write(UPLOAD_FOLDER + "/kad." + filename)
+    return redirect(url_for('uploaded_file', filename="kad." + filename))
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/archi', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return update_archi(filename)
+    return '''
+    <!doctype html>
+    <title>Add Kadaster Repository to Archi File</title>
+    <h1>Upload existing .archimate File</h1>
+    <p>In return you will receive a new .archimate with an (updated) Kadaster
+    Repository and <i>.kad</i> prefix.
+    </p>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=my_port)
-    #app.run(host='0.0.0.0', port=my_port)
-    # print(get_delays())
