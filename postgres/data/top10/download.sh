d78dcbd61a3280fd4f456ab95f0c7f09552dc908
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-09-30 19:09:09 +0000

    bgt/brk load files

---
+++ postgres/data/top10/download.sh
@@ -0,0 +1,4 @@
+curl http://geodata.nationaalgeoregister.nl/top10nlv2/extract/chunkdata/top10nl_gml_filechunks.zip?formaat=gml --output top10.zip
+unzip -o top10.zip
+
+#./load.sh
--- postgres/data/top10/load.sh
+++ postgres/data/top10/load.sh
@@ -1,23 +1,24 @@
 PORT=30779
 HOST=localhost
 PGU=tom
-DB=top50
+DB=top10
 
-SRC=TOP50NL_GML_Filechunks_*/Top50NL_GML_Filechuncks
+SRC=TOP10NL_GML_Filechuncks_*/TOP10NL_GML_Filechuncks/
 
 dropdb -U $PGU -h $HOST -p $PORT $DB
 createdb -U $PGU -h $HOST -p $PORT $DB
 echo "create extension postgis;" | psql -U postgresadmin -h $HOST -p $PORT $DB
 
-rm -f top50.gfs
+rm -f top10.gfs
 
 awk '
 {
      PREV=$0
-     if (NR <= 2 || ($0 !~ "top50nl:FeatureCollectionTop50" && $1 != "<?xml")) print $0
+     if (NR <= 2 || ($0 !~ "top10nl:FeatureCollectionT10NL" && $1 != "<?xml")) print $0
 }
-END { print PREV}' $SRC/*.gml > top50.gml
-for f in top50.gml # $SRC/*.gml
+END { print PREV}' $SRC/*.gml > top10.gml
+
+for f in top10.gml # $SRC/*.gml
 do
   TAB=$(echo "$f" | sed -e "s/bgt_//" -e "s/\\.gml.*//")
   echo $TAB starts at $(date)
@@ -28,6 +29,6 @@ do
 
 done
 
-rm -rf TOP50NL* top50.g*
+rm -rf TOP10NL* top10.g*
 
 exit 0
