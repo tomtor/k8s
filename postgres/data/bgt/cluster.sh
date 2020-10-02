c4ea5114667470f104b06145931fff9fb22b23ab
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-10-02 16:31:10 +0000

    update cluster script

--- postgres/data/bgt/cluster.sh
+++ postgres/data/bgt/cluster.sh
@@ -9,11 +9,12 @@ do
 
   echo $(date) start clustering: $TAB
 
-  echo "CREATE INDEX ${TAB}_wkb_geometry_hash_idx ON public.$TAB (ST_GeoHash(ST_Transform(wkb_geometry,4326)));" | psql -U $PGU -h $HOST -p $PORT $DB
+  #echo "CREATE INDEX ${TAB}_wkb_geometry_hash_idx ON public.$TAB (ST_GeoHash(ST_Transform(wkb_geometry,4326)));" | psql -U $PGU -h $HOST -p $PORT $DB
+  echo "drop INDEX ${TAB}_wkb_geometry_hash_idx;" | psql -U $PGU -h $HOST -p $PORT $DB
 
-  echo "CLUSTER VERBOSE public.$TAB USING ${TAB}_wkb_geometry_hash_idx;" | psql -U $PGU -h $HOST -p $PORT $DB
+  echo "CLUSTER VERBOSE public.$TAB USING ${TAB}_wkb_geometry_geom_idx;" | psql -U $PGU -h $HOST -p $PORT $DB
 
-  echo "analyze verbose public.$TAB;" | psql -U $PGU -h $HOST -p $PORT $DB
+  #echo "analyze verbose public.$TAB;" | psql -U $PGU -h $HOST -p $PORT $DB
 
   echo $(date) end clustering of $TAB
   echo
