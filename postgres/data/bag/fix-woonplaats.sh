4b458d8dc0ec00a8e5399f177e73fa269c727f7e
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-10-11 16:09:11 +0000

    update fix woonplaats

--- postgres/data/bag/fix-woonplaats.sh
+++ postgres/data/bag/fix-woonplaats.sh
@@ -6,3 +6,4 @@ DB=bgt
 echo "update bag_woonplaats set woonplaatsgeometrie_multisurface = st_multi(st_geomfromgml(woonplaatsgeometrie__surface)) where woonplaatsgeometrie_multisurface is null and woonplaatsgeometrie__surface is not NULL;" | psql -U $PGU -h $HOST -p $PORT $DB
 
 echo "ALTER TABLE public.bag_woonplaats DROP COLUMN woonplaatsgeometrie__surface;" | psql -U $PGU -h $HOST -p $PORT $DB
+echo "vacuum full public.bag_woonplaats;" | psql -U $PGU -h $HOST -p $PORT $DB
