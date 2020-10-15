643d9eceb9432098c4f69f310212ba14eafa935d
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-10-15 09:53:19 +0000

    Cleanup stress.sh

--- rapi/stress.sh
+++ rapi/stress.sh
@@ -1,5 +1,12 @@
+while :
+do
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
 curl localhost:8000/lokaalid/[1-1000] -s -o /dev/null &
-curl localhost:8000/lokaalid/[1-1000] -s -o /dev/null &
-curl localhost:8000/lokaalid/[1-1000] -s -o /dev/null &
-curl localhost:8000/lokaalid/[1-1000] -s -o /dev/null &
-curl localhost:8000/lokaalid/[1-1000] -s -o /dev/null
+done
+
+wait
+date 
+
+done
