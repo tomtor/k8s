9a02d1a4a6c8b19e3311b0bdc7965e09fb81ad08
Tom Vijlbrief <tvijlbrief@gmail.com>
2021-10-22 11:22:02 +0000

    skosmos and egeria added

---
+++ make-cert.sh
@@ -0,0 +1,6 @@
+microk8s.disable ingress
+
+sudo certbot certonly --standalone --cert-name  tom-kad-k8s.westeurope.cloudapp.azure.com -d k8s.v7f.eu -d pgadmin.v7f.eu -d egeria.v7f.eu -d tom-kad-k8s.westeurope.cloudapp.azure.com -d skosmos.v7f.eu
+
+microk8s.enable ingress
+sh renew-cert.sh
