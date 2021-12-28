f51210a57b8a15fa728ae391fad281faa2d0df9c
Tom Vijlbrief <tvijlbrief@gmail.com>
2021-12-28 10:07:51 +0000

    Improved spatial filter test

--- jena/query.sh
+++ jena/query.sh
@@ -2,7 +2,7 @@ export PGHOST=localhost
 export PGPORT=30779
 
 ###echo 'drop table pand10; select gml_id, st_transform(ST_GeometryN(pand.wkb_geometry,1),4326) as geometry, pand.wkb_geometry, st_area(ST_GeometryN(pand.wkb_geometry,1)) as area into pand10 from pand where st_intersects(st_transform(pand.wkb_geometry,4326), ST_MakeEnvelope(5, 52, 5.1, 52.1, 4326))' | psql -A -t bgt
-#echo 'drop table pand10; select gml_id, st_transform(pand.wkb_geometry,4326) as geometry, pand.wkb_geometry, st_area(pand.wkb_geometry) as area into pand10 from pand where st_intersects(st_transform(pand.wkb_geometry,4326), ST_MakeEnvelope(5, 52, 5.5, 52.5, 4326))' | psql -A -t bgt
+#echo 'drop table pand10; select gml_id, st_transform(pand.wkb_geometry,4326) as geometry, pand.wkb_geometry, st_area(pand.wkb_geometry) as area into pand10 from pand where st_intersects(st_transform(pand.wkb_geometry,4326), ST_MakeEnvelope(5, 52, 5.2, 52.2, 4326))' | psql -A -t bgt
 
 echo 'CREATE INDEX IF NOT EXISTS pand10idx ON public.pand10 USING gist (geometry);' | psql -A -t bgt
 echo 'CREATE INDEX IF NOT EXISTS pand10wkbidx ON public.pand10 USING gist (wkb_geometry);' | psql -A -t bgt
@@ -19,6 +19,6 @@ a.gml_id != b.gml_id and a.area > 1000 and
 ST_DWithin(a.wkb_geometry, b.wkb_geometry, 10)
 -- and st_distance(a.wkb_geometry,b.wkb_geometry) < 10' | psql -A -t bgt
 
-#curl "http://localhost:3030/ds?query=$(jq -sRr @uri query.rq)" | fgrep float | wc
+curl "http://localhost:3030/ds?query=$(jq -sRr @uri query.rq)" | fgrep float | wc
 
 time curl "http://localhost:3030/ds?query=$(jq -sRr @uri scan.rq)" | fgrep float | wc
